28379baa7c67dd31ff0bb06d6d73b1d3c3af46dd
dangyihao <1335902682@qq.com>
2024-09-13 18:01:08 +0800

    feat: 完成不同模块打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,17 @@ const jsonplugin = require('@rollup/plugin-json')
 
 
 module.exports = {
-  input: './main.js',
-  output: {
+    input: './main.js',
+    output: [
+        {
     file: 'dist/lib/index.js',
       format: 'esm' // 有cjs 和 esm两种模式
-  },
+        },
+        {
+            file: 'dist/es/index.js',
+      format: 'esm' // 有cjs 和 esm两种模式
+        }
+  ],
   plugins: [
     //   nodeResolve(), // 该插件是将文件中引入的第三方包也打包进dist中，rollup默认是不会将三方包打包的
       commonjs(), // 该插件是将commonjs模块转换为esm模块，再format为esm时且文件中有cjs语法时有用
